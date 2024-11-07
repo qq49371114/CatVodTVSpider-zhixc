8c4b40ea0cf34d9e0617c498da033edabec3a1c5
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-11-07 23:27:17 +0800

    更新 Init.java

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -7,6 +7,6 @@ import com.github.catvod.crawler.SpiderDebug;
 public class Init {
     public static void init(Context context) {
         SpiderDebug.log("自定义爬虫代码加载成功！");
-        Notify.show("🐯遥遥领先专用资源！资源来源于网络免费爬取。🐯");
+        
     }
 }
